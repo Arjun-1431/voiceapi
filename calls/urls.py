7177bf78bdb65ca9ab8441aa5b5d84b048a75244
Arjun Singh <116427147+Arjun-1431@users.noreply.github.com>
2025-02-12 15:34:48 +0530

    Update urls.py

--- calls/urls.py
+++ calls/urls.py
@@ -1,8 +1,19 @@
+# from django.urls import path
+# from .views import *
+
+# urlpatterns = [
+#     path('make_call/', make_outbound_call, name='make_outbound_call'),
+#     path('handle_call/', handle_call, name='handle_call'),
+#     path('call/', call_page, name='call_page'),
+#     path('get_twilio_token/', get_twilio_token, name='get_twilio_token'), 
+
+# ]
+
 from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', index, name='home'),  # Root URL को `/` से map करें
     path('make_call/', make_outbound_call, name='make_outbound_call'),
     path('handle_call/', handle_call, name='handle_call'),
     path('call/', call_page, name='call_page'),
