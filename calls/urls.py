d932753bba4f545e4dda6264a4f7e88a225b60b3
Arjun Singh <116427147+Arjun-1431@users.noreply.github.com>
2025-02-12 19:42:49 +0530

    Update urls.py

--- calls/urls.py
+++ calls/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('make_call/', make_outbound_call, name='make_outbound_call'),
     path('handle_call/', handle_call, name='handle_call'),
-    path('call/', call_page, name='call_page'),
+    path('', call_page, name='call_page'),
     path('get_twilio_token/', get_twilio_token, name='get_twilio_token'), 
 
 ]
